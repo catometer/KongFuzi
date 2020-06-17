bf3f71629b6c33039429cd49f14bf1027fee71d1
Ani Vardanyan <aniwardanjan@gmail.com>
2020-06-17 21:12:16 +0300

    Brushing up the code

--- kfgui2.py
+++ kfgui2.py
@@ -23,99 +23,137 @@ from tkinter import filedialog
 from tkinter import messagebox
 
 ttl = "KongFuzi: Version 2.0"
-counter = {"correct":0,"wrong":0,"incomplete":0,"partially wrong":0}
-tonals = {"a1":"ā","a2":"á","a3":"ǎ","a4":"à","e1":"ē","e2":"é","e3":"ě","e4":"è","i1":"ī","i2":"í","i3":"ǐ","i4":"ì","o1":"ō","o2":"ó","o3":"ǒ","o4":"ò","u1":"ū","u2":"ú","u3":"ǔ","u4":"ù","v1":"ǖ","v2":"ǘ","v3":"ǚ","v4":"ǜ"}
+
+#Storing the number of correct/wrong/incomplete answers to display as feedback by the end of the practice session
+stats_counter = {"correct":0,"wrong":0,"incomplete":0,"partially wrong":0}
+
+#Pinyin characters for chinese tonal sounds (values) and non-pinyin conventions (keys) to type them in within the program
+tonals = {"a1":"ā","a2":"á","a3":"ǎ","a4":"à"
+          ,"e1":"ē","e2":"é","e3":"ě","e4":"è"
+          ,"i1":"ī","i2":"í","i3":"ǐ","i4":"ì"
+          ,"o1":"ō","o2":"ó","o3":"ǒ","o4":"ò"
+          ,"u1":"ū","u2":"ú","u3":"ǔ","u4":"ù"
+          ,"v1":"ǖ","v2":"ǘ","v3":"ǚ","v4":"ǜ"
+         }
+
+#Feedback message options for users' answers 
+fbdict = {"correct":['Correct!','Great job!','Keep it up!','Yes!']
+          ,"wrong":['Wrong! Try again!','Oops!','Nope!']
+          ,"incomplete":"Correct, but it has more meanings!"}
+
 run = True
-def stats(dct):
-    text = ""
-    for key in dct:
-        val = counter.get(key)
-        if val == 1:
-            line = str(val)+" "+str(key)+" answer\n"
-        else:
-            line = str(val)+" "+str(key)+" answers\n"
-        text = text+line
-    return text
 
-def _hint(correct_a):
-    word = list(correct_a)
-    hint = "none"
-    if correct_a.startswith('to be '):
-        hint = "It starts with '(to be ) "+str(word[6])+"'"
-    elif correct_a.startswith('to '):
-        hint = "It starts with '(to) "+str(word[3])+"'"
-    elif correct_a.startswith('a '):
-        hint = "It starts with '(a) "+str(word[2])+"'"
-    elif correct_a.startswith('an '):
-        hint = "It starts with '(a) "+str(word[3])+"'"
-    elif correct_a.startswith('the '):
-        hint = "It starts with '(a) "+str(word[4])+"'"
-    else:
-        hint = "It starts with '"+str(word[0])+"'!"
-    messagebox.showinfo(ttl,hint)
-
-def _feedback(correct_a,user_a_entry,window):
-    user_a_raw = user_a_entry.get().lower()
-    user_a = user_a_raw
-    for key,val in tonals.items():
-        if key in user_a_raw:
-            user_a = user_a.replace(key,val)
-    fbdict = {"correct":['Correct!','Great job!','Keep it up!','Yes!'],"wrong":['Wrong! Try again!','Oops!','Nope!'],"incomplete":"Correct, but it has more meanings!"}
-    if correct_a == user_a:
-        feedback = random.choice(fbdict["correct"])
-        counter["correct"] += 1
-    elif correct_a != user_a and "," in correct_a and "," in user_a:
-        correct_a = correct_a.split(', ')  
-        correct_a.sort()
-        user_a = user_a.split(', ')
-        user_a.sort()
-        if correct_a == user_a:
-            feedback = random.choice(fbdict["correct"])
-            counter["correct"] += 1
-        else:
-            truefalse = []
-            for n in user_a:
-                if n in correct_a:
-                    value = "true"
-                    truefalse.append(value)
-                if n not in correct_a:
-                    value = "false"
-                    truefalse.append(value)
-            if "false" in truefalse and "true" in truefalse:
-                feedback = "One or several answers are wrong."
-                counter["partially wrong"] += 1
-            elif "false" in truefalse:
-                feedback = random.choice(fbdict["wrong"])
-                counter["wrong"] += 1
+def mainmenu():
+    startwindow = Tk()
+    startwindow.title(ttl)
+    Label(startwindow, text="Hello! KongFuzi is here to help you with your Chinese vocabulary of the day! Choose an option to continue.\n",wraplength=400,width=60).grid(row=0)
+    b1_select = Button (startwindow, text="Select a file", width=13, command=lambda:select(startwindow))
+    b1_select.grid(row=1)
+    b2_input = Button (startwindow, text="Input vocabulary", width=16, command=lambda:vocinput(startwindow))
+    b2_input.grid(row=2)
+    Label(startwindow,text="\n").grid(row=3)
+    startwindow.mainloop()
+    
+def backtomain(vocabulary,previouswindow):
+    if len(vocabulary) == 1:
+        if vocabulary[0] != "input":
+            yesno = messagebox.askquestion(ttl, "Do you want to save your current submissions?")
+            if yesno == "yes":
+                save(vocabulary)
+                previouswindow.destroy()
+                mainmenu()
             else:
-                feedback = str(fbdict["incomplete"])
-                counter["incomplete"] += 1
-    elif correct_a != user_a and "," in correct_a:
-        correct_a = correct_a.split(', ')
-        if user_a in correct_a:
-            feedback = str(fbdict["incomplete"])
-            counter["incomplete"] += 1
+                previouswindow.destroy()
+                mainmenu()
         else:
-            feedback = random.choice(fbdict["wrong"])
-            counter["wrong"] += 1
+            previouswindow.destroy()
+            mainmenu()
     else:
-        feedback = random.choice(fbdict["wrong"])
-        counter["wrong"] += 1
-    finalfeedback = feedback+"\nYour answer: "+str(user_a)+"\nCorrect answer: "+str(correct_a)
-    messagebox.showinfo(ttl,finalfeedback)
-    user_a_entry.delete(0,END)
-    window.destroy()
+        save(vocabulary)
+        previouswindow.destroy()
+        mainmenu()
 
-def _exit():
-    global run
-    run = False
-    msg = stats(counter)
-    x = messagebox.showinfo(ttl,msg)
-    if x == "ok":
-        exit()
+def select(previous_window):
+    fchoice = filedialog.askopenfilename(title="Select a vocabulary file:",defaultextension="txt")
+    try:
+        handler = open(fchoice,encoding="utf-8-sig")
+    except: "do nothing!"
+    else:
+        vocab = []
+        for line in handler:
+            line = line.strip().split(' - ')
+            vocab.append(line)
+        test(vocab,previous_window)
 
-#recursive function :)
-        
+
+def vocinput(previouswindow):
+    vocab = ["input"]
+    previouswindow.destroy()
+    vocinpwin = Tk()
+    vocinpwin.title(ttl)
+    subttl = Label(text = "VOCABULARY INPUT",width=60)
+    subttl.grid(row=0,column=0,columnspan=3)
+    hanzi_request = Label(text = "Hanzi: ")
+    hanzi_request.grid(row=1,column=0,sticky="e")
+    hanzi_entry = Entry(vocinpwin)
+    hanzi_entry.grid(row=1,column=1,stick="w")
+    pinyin_request = Label(text = "Pinyin: ")
+    pinyin_request.grid(row=2,column=0,sticky="e")
+    pinyin_entry = Entry(vocinpwin)
+    pinyin_entry.grid(row=2,column=1,sticky="w")
+    pinyin_info = Label(text = "Note: for tonal marks type 1, 2, 3 or 4 after the vowel.\nFor 'u' with umlaut type 'v' followed by the tone number.\nE.g. 'e3' for 'ě' or 'v2' for 'ǘ'.",justify="left")
+    pinyin_info.grid(row=3,column=1,sticky="e")
+    trans_request = Label(text = "Meaning: ")
+    trans_request.grid(row=4,column=0,sticky="e")
+    trans_entry = Entry(vocinpwin)
+    trans_entry.grid(row=4,column=1,sticky="w")
+    showvoc = Text(vocinpwin)
+    sbmitbtn = Button(text = "Submit", command=lambda:lineappend(hanzi_entry,pinyin_entry,trans_entry,showvoc,vocab),width=16)
+    sbmitbtn.grid(row=5,column=1,sticky="w")
+    savebtn = Button(text = "Save", command=lambda:save(vocab),width=16)
+    savebtn.grid(row=5,column=2,sticky="e")
+    testbtn = Button(text = "Go to test",command=lambda:test(vocab,vocinpwin),width=16)
+    testbtn.grid(row=1,column=2,sticky="e")
+    backbtn = Button(text = "Main Menu",width=16,command=lambda:backtomain(vocab,vocinpwin))
+    backbtn.grid(row=2,column=2,sticky="e")
+    showvoc.grid(row=10,column=0,columnspan=3)
+    vocinpwin.mainloop()
+
+#helper function for lineappend
+def getfrom(lst):
+    errormsg = 0
+    line = []
+    for i in lst:
+        e = i.get().lower()
+        e = e.strip()
+        if e != "":
+            if lst.index(i) == 1:
+                tonals_added = e
+                for key,val in tonals.items():
+                    if key in e:
+                        tonals_added = tonals_added.replace(key,val)
+                line.append(tonals_added)
+            else:
+                line.append(e)
+        else:
+            if errormsg == 0:
+                errormsg = 1
+                messagebox.showinfo(ttl,"One or more fields are empty!")
+    return line
+
+def lineappend(eh,ep,et,textbox,vocabulary):
+    rawline = [eh,ep,et]    
+    extracted = getfrom(rawline)
+    if len(extracted) == 3:
+        vocabulary.append(extracted)
+        for i in rawline:
+            i.delete(0, END)
+    textbox.delete(0.0,END)
+    for finalline in vocabulary:
+        if finalline != "input":
+            finalline = str(finalline[0])+" - "+str(finalline[1])+" - "+str(finalline[2])+"\n"
+            textbox.insert(END,finalline)
+
 def save(vocabulary):
     if vocabulary[0] == "input":
         if len(vocabulary) == 1:
@@ -134,8 +172,6 @@ def save(vocabulary):
                 vocabfile.write(i[0]+" - "+i[1]+" - "+i[2]+"\n")
             vocabfile.close()
 
-#another recursive functon :P
-                
 def test(vocabulary,previous_window):
     if vocabulary[0] == "input":
         if len(vocabulary) == 1:
@@ -183,114 +219,94 @@ def test(vocabulary,previous_window):
             type_trans = "TRANSLATION"
             testwindow(type_pinyin,pinyin)
             if run:
-                testwindow(type_trans,trans)    
+                testwindow(type_trans,trans)
 
-def select(previous_window):
-    fchoice = filedialog.askopenfilename(title="Select a vocabulary file:",defaultextension="txt")
-    try:
-        handler = open(fchoice,encoding="utf-8-sig")
-    except: "do nothing!"
+def _hint(correct_a):
+    word = list(correct_a)
+    hint = "none"
+    if correct_a.startswith('to be '):
+        hint = "It starts with '(to be ) "+str(word[6])+"'"
+    elif correct_a.startswith('to '):
+        hint = "It starts with '(to) "+str(word[3])+"'"
+    elif correct_a.startswith('a '):
+        hint = "It starts with '(a) "+str(word[2])+"'"
+    elif correct_a.startswith('an '):
+        hint = "It starts with '(a) "+str(word[3])+"'"
+    elif correct_a.startswith('the '):
+        hint = "It starts with '(a) "+str(word[4])+"'"
     else:
-        vocab = []
-        for line in handler:
-            line = line.strip().split(' - ')
-            vocab.append(line)
-        test(vocab,previous_window)
+        hint = "It starts with '"+str(word[0])+"'!"
+    messagebox.showinfo(ttl,hint)
 
-def lineappend(eh,ep,et,textbox,vocabulary):
-    rawline = [eh,ep,et]
-    def getfrom(lst):
-        errormsg = 0
-        line = []
-        for i in lst:
-            e = i.get().lower()
-            e = e.strip()
-            if e != "":
-                if lst.index(i) == 1:
-                    tonals_added = e
-                    for key,val in tonals.items():
-                        if key in e:
-                            tonals_added = tonals_added.replace(key,val)
-                    line.append(tonals_added)
-                else:
-                    line.append(e)
-            else:
-                if errormsg == 0:
-                    errormsg = 1
-                    messagebox.showinfo(ttl,"One or more fields are empty!")
-        return line
-    extracted = getfrom(rawline)
-    if len(extracted) == 3:
-        vocabulary.append(extracted)
-        for i in rawline:
-            i.delete(0, END)
-    textbox.delete(0.0,END)
-    for finalline in vocabulary:
-        if finalline != "input":
-            finalline = str(finalline[0])+" - "+str(finalline[1])+" - "+str(finalline[2])+"\n"
-            textbox.insert(END,finalline)
-
-def vocinput(previouswindow):
-    vocab = ["input"]
-    previouswindow.destroy()
-    vocinpwin = Tk()
-    vocinpwin.title(ttl)
-    subttl = Label(text = "VOCABULARY INPUT",width=60)
-    subttl.grid(row=0,column=0,columnspan=3)
-    hanzi_request = Label(text = "Hanzi: ")
-    hanzi_request.grid(row=1,column=0,sticky="e")
-    hanzi_entry = Entry(vocinpwin)
-    hanzi_entry.grid(row=1,column=1,stick="w")
-    pinyin_request = Label(text = "Pinyin: ")
-    pinyin_request.grid(row=2,column=0,sticky="e")
-    pinyin_entry = Entry(vocinpwin)
-    pinyin_entry.grid(row=2,column=1,sticky="w")
-    pinyin_info = Label(text = "Note: for tonal marks type 1, 2, 3 or 4 after the vowel.\nFor 'u' with umlaut type 'v' followed by the tone number.\nE.g. 'e3' for 'ě' or 'v2' for 'ǘ'.",justify="left")
-    pinyin_info.grid(row=3,column=1,sticky="e")
-    trans_request = Label(text = "Meaning: ")
-    trans_request.grid(row=4,column=0,sticky="e")
-    trans_entry = Entry(vocinpwin)
-    trans_entry.grid(row=4,column=1,sticky="w")
-    showvoc = Text(vocinpwin)
-    sbmitbtn = Button(text = "Submit", command=lambda:lineappend(hanzi_entry,pinyin_entry,trans_entry,showvoc,vocab),width=16)
-    sbmitbtn.grid(row=5,column=1,sticky="w")
-    savebtn = Button(text = "Save", command=lambda:save(vocab),width=16)
-    savebtn.grid(row=5,column=2,sticky="e")
-    testbtn = Button(text = "Go to test",command=lambda:test(vocab,vocinpwin),width=16)
-    testbtn.grid(row=1,column=2,sticky="e")
-    backbtn = Button(text = "Main Menu",width=16,command=lambda:backtomain(vocab,vocinpwin))
-    backbtn.grid(row=2,column=2,sticky="e")
-    showvoc.grid(row=10,column=0,columnspan=3)
-    vocinpwin.mainloop()
-
-def backtomain(vocabulary,previouswindow):
-    if len(vocabulary) == 1:
-        if vocabulary[0] != "input":
-            yesno = messagebox.askquestion(ttl, "Do you want to save your current submissions?")
-            if yesno == "yes":
-                save(vocabulary)
-                previouswindow.destroy()
-                mainmenu()
-            else:
-                previouswindow.destroy()
-                mainmenu()
+def _feedback(correct_a,user_a_entry,window):
+    user_a_raw = user_a_entry.get().lower()
+    user_a = user_a_raw
+    for key,val in tonals.items():
+        if key in user_a_raw:
+            user_a = user_a.replace(key,val)
+    if correct_a == user_a:
+        feedback = random.choice(fbdict["correct"])
+        stats_counter["correct"] += 1
+    elif correct_a != user_a and "," in correct_a and "," in user_a:
+        correct_a = correct_a.split(', ')  
+        correct_a.sort()
+        user_a = user_a.split(', ')
+        user_a.sort()
+        if correct_a == user_a:
+            feedback = random.choice(fbdict["correct"])
+            stats_counter["correct"] += 1
         else:
-            previouswindow.destroy()
-            mainmenu()
+            truefalse = []
+            for n in user_a:
+                if n in correct_a:
+                    value = "true"
+                    truefalse.append(value)
+                if n not in correct_a:
+                    value = "false"
+                    truefalse.append(value)
+            if "false" in truefalse and "true" in truefalse:
+                feedback = "One or several answers are wrong."
+                stats_counter["partially wrong"] += 1
+            elif "false" in truefalse:
+                feedback = random.choice(fbdict["wrong"])
+                stats_counter["wrong"] += 1
+            else:
+                feedback = str(fbdict["incomplete"])
+                stats_counter["incomplete"] += 1
+    elif correct_a != user_a and "," in correct_a:
+        correct_a = correct_a.split(', ')
+        if user_a in correct_a:
+            feedback = str(fbdict["incomplete"])
+            stats_counter["incomplete"] += 1
+        else:
+            feedback = random.choice(fbdict["wrong"])
+            stats_counter["wrong"] += 1
     else:
-        save(vocabulary)
-        previouswindow.destroy()
-        mainmenu()
+        feedback = random.choice(fbdict["wrong"])
+        stats_counter["wrong"] += 1
+    finalfeedback = feedback+"\nYour answer: "+str(user_a)+"\nCorrect answer: "+str(correct_a)
+    messagebox.showinfo(ttl,finalfeedback)
+    user_a_entry.delete(0,END)
+    window.destroy()
 
-def mainmenu():
-    startwindow = Tk()
-    startwindow.title(ttl)
-    Label(startwindow, text="Hello! KongFuzi is here to help you with your Chinese vocabulary of the day! Choose an option to continue.\n",wraplength=400,width=60).grid(row=0)
-    b1_select = Button (startwindow, text="Select a file", width=13, command=lambda:select(startwindow))
-    b1_select.grid(row=1)
-    b2_input = Button (startwindow, text="Input vocabulary", width=16, command=lambda:vocinput(startwindow))
-    b2_input.grid(row=2)
-    Label(startwindow,text="\n").grid(row=3)
-    startwindow.mainloop()
+#Function for displaying a message about the number of correct/wrong/incomplete answers by the end of the practice session 
+def stats(dct):
+    text = ""
+    for key in dct:
+        val = stats_counter.get(key)
+        if val == 1:
+            line = str(val)+" "+str(key)+" answer\n"
+        else:
+            line = str(val)+" "+str(key)+" answers\n"
+        text = text+line
+    return text
+
+def _exit():
+    global run
+    run = False
+    msg = stats(stats_counter)
+    x = messagebox.showinfo(ttl,msg)
+    if x == "ok":
+        exit()
 
 mainmenu()
